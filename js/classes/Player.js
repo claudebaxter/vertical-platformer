39867f222324a1e0ebf63104020915ba1a8532ea
elborracho420 <e.baxter@dark-coin.io>
2023-05-08 10:55:44 -0500

    Adjusting hitbox to fit around player sprite image.

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -39,11 +39,11 @@ class Player extends Sprite {
     updateHitbox() {
         this.hitbox = {
             position: {
-                x: this.position.x,
-                y: this.position.y
+                x: this.position.x + 34,
+                y: this.position.y + 26
             },
-            width: 10,
-            height: 10
+            width: 14,
+            height: 27
         }
     }
     checkForHorizontalCollisions() {
